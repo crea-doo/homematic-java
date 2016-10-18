c7a2551edd8b5cbb6da305d5b59e72140502e374
crea-doo <development@crea-doo.at>
2016-10-18 17:10:51 +0200

    Working on AES en-/decryption

--- src/main/java/at/creadoo/homematic/socket/Test.java
+++ src/main/java/at/creadoo/homematic/socket/Test.java
@@ -5,10 +5,18 @@ import java.net.InetSocketAddress;
 public class Test {
 
 	public static void main(String[] args) {
+		/* */
 		final SocketLink hardware = new SocketLink(new InetSocketAddress("192.168.0.1", 1000));
 		hardware.setAESEnabled(true);
 		hardware.setAESLANKey("00112233445566778899AABBCCDDEEFF");
 		hardware.setAESRFKey("00112233445566778899AABBCCDDEEFF");
+		/* */
+
+		/* /
+		final SocketLink hardware = new SocketLink(new InetSocketAddress("192.168.0.2", 1000));
+		hardware.setAESEnabled(false);
+		hardware.setAESLANKey("00112233445566778899AABBCCDDEEFF");
+		/* */
 		
 		try {
 			hardware.start();
